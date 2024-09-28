976d7867422460c27452784745e5155427a82f01
craigchihombe <achihombe@gmail.com>
2024-09-28 10:20:14 +0200

    Add files via upload

---
+++ operations on set.py
@@ -0,0 +1,18 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1.0,"Money",(1,2,3,4,5)}
+print(my_set)
+
+my_set = {1,2,3,4,5,3,6,4,1,4}
+print(my_set)
+
+my_set = set([1,2,3,4,5,6,2,4,])
+print(my_set)
+
+my_set = set([0,1,2,3,4,5,6,7])
+print("original set")
+print(my_set)
+my_set.pop()
+print("after removing the first element from the set.")
+print(my_set)
